3b40da63c3850b547da06e58c51148d9aa0e028c
Fredrik Ridderheim <fredrik.ridderheim@hotmail.com>
2016-12-21 22:18:29 +0100

    Testing TDD

---
+++ TDD_Test/TDD_Test.Library.Tests/FizzBusserTests.cs
@@ -0,0 +1,41 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Test.Library.Tests
+{
+    [TestFixture]
+    public class FizzBusserTests
+    {
+        [Test]
+        public void Buzzer_whenDefault_returnsInput([Values(1, 2, 4, 7, 8, 11, 13, 14, 16, 17, 19)] int input)
+        {
+            string output = FizzBusser.GetValue(input);
+            Assert.AreEqual(input.ToString(), output);
+        }
+
+        [Test]
+        public void Buzzer_whenDiv3_returnsFizz([Values(3, 6, 9, 12, 18)] int input)
+        {
+            string output = FizzBusser.GetValue(input);
+            Assert.AreEqual("Fizz", output);
+        }
+
+        [Test]
+        public void Buzzer_whenDiv5_returnsBuzz([Values(5, 10, 20)] int input)
+        {
+            string output = FizzBusser.GetValue(input);
+            Assert.AreEqual("Buzz", output);
+        }
+        
+        [Test]
+        public void Buzzer_whenDiv3andDiv5_returnsFizzBuzz([Values(15)] int input)
+        {
+            string output = FizzBusser.GetValue(input);
+            Assert.AreEqual("FizzBuzz", output);
+        }
+    }
+}
---
+++ TDD_Test/TDD_Test.Library/FizzBusser.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TDD_Test.Library
+{
+    public class FizzBusser
+    {
+        public static string GetValue(int input)
+        {
+            string output = string.Empty;
+
+            if (input % 3 == 0)
+            {
+                output += "Fizz";
+            }
+            if (input % 5 == 0)
+            {
+                output += "Buzz";
+            }
+            if (string.IsNullOrEmpty(output))
+            {
+                output = input.ToString();
+            }
+            return output;
+        }
+    }
+}
